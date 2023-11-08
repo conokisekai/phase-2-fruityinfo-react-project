b6923645e8a15966edcfd515e183ae41d2b1c7bb
Conrad Kambi <kambiconrad@gmail.com>
2023-11-08 22:21:22 +0300

    Update more components

--- src/App/Components/FruitList/FruitList.jsx
+++ src/App/Components/FruitList/FruitList.jsx
@@ -1,2 +1,34 @@
+import React, { useState, useEffect } from 'react';
+
+const FruitList = () => {
+  const [fruits, setFruits] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://fruityvice.com/api/fruit/all');
+        const data = await response.json();
+        setFruits(data); 
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData(); 
+  }, []); 
+
+  return (
+    <div>
+      <h2>Fruit List</h2>
+      <ul>
+        {fruits.map((fruit) => (
+          <li key={fruit.id}>{fruit.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default FruitList;
 
 
